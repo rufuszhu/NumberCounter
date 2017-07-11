89eea50e0a0f587a93534eb8701ba90c49fb5577
Rufus Zhu <rzhu@pof.com>
2017-07-10 17:52:48 -0700

    animation works

--- app/src/main/java/com/example/rzhu/counter/CounterNumberView.java
+++ app/src/main/java/com/example/rzhu/counter/CounterNumberView.java
@@ -5,12 +5,13 @@ import android.animation.AnimatorSet;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,7 +21,7 @@ import butterknife.ButterKnife;
  * Created by rzhu on 5/31/2017.
  */
 
-public class CounterNumberView extends RelativeLayout
+public class CounterNumberView extends FrameLayout
 {
 	@BindView(R.id.number_old)
 	TextView mOldNumber;
@@ -33,22 +34,35 @@ public class CounterNumberView extends RelativeLayout
 	@BindView(R.id.lower_cover)
 	ImageView mLowerCover;
 
-
 	private AnimatorSet mAnimateOut;
 	private AnimatorSet mAnimateIn;
+	private int mHeight;
+	private int mWidth;
+	private Handler mHandler;
+
 
 	public CounterNumberView(@NonNull Context context, @Nullable AttributeSet attrs)
 	{
 		super(context, attrs);
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(R.layout.counter_number_view, this, true);
+
+		mAnimateOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.out_animation);
+		mAnimateIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.in_animation);
+		mHandler = new Handler();
+	}
+
+	@Override
+	protected void onFinishInflate()
+	{
+		super.onFinishInflate();
 		ButterKnife.bind(this);
-		int distance = 1000;
+
+		int distance = 8000;
 		float scale = getResources().getDisplayMetrics().density * distance;
 		mOldNumber.setCameraDistance(scale);
 		mNewNumber.setCameraDistance(scale);
-		mAnimateOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.out_animation);
-		mAnimateIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.in_animation);
+
 	}
 
 	/**
@@ -59,40 +73,56 @@ public class CounterNumberView extends RelativeLayout
 		if (mNewNumber.getText().toString().isEmpty())
 		{
 			mNewNumber.setText(String.valueOf(newNumber));
+			return;
 		}
 
-		int oldNumber = Integer.valueOf(mNewNumber.getText().toString());
+		final int oldNumber = Integer.valueOf(mNewNumber.getText().toString());
 		if (oldNumber != newNumber)
 		{
-			mOldNumber.setVisibility(VISIBLE);
-			mNewNumber.setVisibility(VISIBLE);
-
+			mNumberBehind.setText(String.valueOf(newNumber));
 			mOldNumber.setText(String.valueOf(oldNumber));
-			int numberHeight = mOldNumber.getMeasuredHeight();
-			int numberWidth = mOldNumber.getMeasuredWidth();
+			mNewNumber.setText(String.valueOf(newNumber));
 
-			//take snapshot of upper half of number view
-			Bitmap upperHalfNewNumber = Bitmap.createBitmap(numberWidth, numberHeight/2, Bitmap.Config.ARGB_8888);
-			Canvas c = new Canvas(upperHalfNewNumber);
-			mNewNumber.layout(0, 0, numberWidth, numberHeight / 2);
-			mNewNumber.draw(c);
+			if (mWidth == 0 || mHeight == 0)
+			{
+				mHeight = mOldNumber.getMeasuredHeight();
+				mWidth = mOldNumber.getMeasuredWidth();
+			}
 
-			Bitmap lowerHalfOldNumber = Bitmap.createBitmap(numberWidth, numberHeight/2, Bitmap.Config.ARGB_8888);
+			//take snapshot of upper half of new number
+			Bitmap upperHalfNewNumber = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+			Canvas upperCanvas = new Canvas(upperHalfNewNumber);
+			upperCanvas.clipRect(0, 0, mWidth, mHeight / 2);
+			mNewNumber.draw(upperCanvas);
+
+			//take snapshot of lower half of old number
+			Bitmap lowerHalfOldNumber = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
 			Canvas lowerCanvas = new Canvas(lowerHalfOldNumber);
-			mOldNumber.layout(0, numberHeight / 2, numberWidth, numberHeight);
+			lowerCanvas.clipRect(0, mHeight / 2, mWidth, mHeight);
 			mOldNumber.draw(lowerCanvas);
 
 			mUpperCover.setImageBitmap(upperHalfNewNumber);
 			mLowerCover.setImageBitmap(lowerHalfOldNumber);
 
+			mUpperCover.setVisibility(GONE);
+			mLowerCover.setVisibility(VISIBLE);
 
-			mOldNumber.setVisibility(GONE);
-			mNewNumber.setVisibility(GONE);
+			mHandler.postDelayed(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					mNumberBehind.setText(String.valueOf(oldNumber));
+					mUpperCover.setVisibility(VISIBLE);
+					mLowerCover.setVisibility(GONE);
+				}
+			}, getResources().getInteger(R.integer.counter_animation_half_length));
 
-//			mAnimateOut.setTarget(mOldNumber);
-//			mAnimateIn.setTarget(mNewNumber);
-//			mAnimateOut.start();
-//			mAnimateIn.start();
+
+			mAnimateOut.setTarget(mOldNumber);
+			mAnimateIn.setTarget(mNewNumber);
+			mAnimateOut.start();
+			mAnimateIn.start();
 		}
 	}
 }
